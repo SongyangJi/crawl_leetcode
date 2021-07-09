2fb42faa41acab74036a54f1c632060bc8f38085
SongyangJi <1539242799@qq.com>
2021-07-10 02:03:29 +0800

    增加了题目难度分类数量、通过率和题解数量关系示意图

--- store/analyze.py
+++ store/analyze.py
@@ -17,26 +17,39 @@ c1 = db.problems_brief_information
 c2 = db.problems_raw
 
 
-def getDataFrameFromProblems_brief_information():
-    data_model = c1.find_one()
-    df = pd.DataFrame(columns=list(data_model))
+def getData():
     ls = []
     for p in c1.find():
         ls.append(p)
-    df = df.append(ls, ignore_index=True)
+    return ls
+
+
+def getDataFrameFromProblems_brief_information():
+    data_model = c1.find_one()
+    df = pd.DataFrame(columns=list(data_model))
+    df = df.append(getData(), ignore_index=True)
     return df
 
 
-def analyzeDataFrame(df):
+def analyzeLevel_Num(df):
     df = df.groupby('difficult_level')['_id'].count()
     df = pd.DataFrame(df.values, columns=['数量'], index=df.index)
-    print(df)
     df.plot(kind='bar')
     plt.show()
     pass
 
 
+def analyzeSolutions_Rate():
+    data = getData()
+    df = pd.DataFrame({'通过率': [float(it['passing_rate'][:-1])/100 for it in data]},
+                      index=[it['solutions'] for it in data])
+    print(df)
+    df.sort_index().plot()
+    plt.show()
+    pass
+
 
 if __name__ == '__main__':
     df = getDataFrameFromProblems_brief_information()
-    analyzeDataFrame(df)
+    analyzeLevel_Num(df)
+    analyzeSolutions_Rate()
